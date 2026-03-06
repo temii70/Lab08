e7fcd79c583a0cdac2a3dca6650a024aa9c10672
Temii70 <otun@ualberta.ca>
2026-03-06 16:09:02 -0700

    passed deletecity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,8 +18,16 @@ public class CustomList {
         return cities.contains(city);
     }
 
+    /**
+     * This deletes a city from the list if it exists
+     * @param city The city to delete
+     */
     public void deleteCity(City city) {
-        // Red Phase: doing nothing so the test fails
+        if (cities.contains(city)) {
+            cities.remove(city);
+        } else {
+            throw new IllegalArgumentException("City not found");
+        }
     }
 
     public int countCities() {
